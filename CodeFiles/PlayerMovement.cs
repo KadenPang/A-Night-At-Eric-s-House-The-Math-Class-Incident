a04a725a98e9b813b10148fe41e8a89f81640b91
chingistoktamyssov <125822105+chingistoktamyssov@users.noreply.github.com>
2024-04-05 12:12:39 -0400

    Update PlayerMovement.cs

--- CodeFiles/PlayerMovement.cs
+++ CodeFiles/PlayerMovement.cs
@@ -2,26 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// old version of player movement, allows for 8 directional movement
+
 public class NewPlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
-
     Vector2 movement;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void Update()
+    
     {
+    // get user input for either horizontal movement keys (left right) or vertical movement keys (up down)
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
-    // Update is called once per frame
+    // update is called once per frame
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
