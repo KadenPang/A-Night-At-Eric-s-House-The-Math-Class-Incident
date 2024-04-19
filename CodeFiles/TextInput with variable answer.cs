611ba5b62114c15433bb8aaa3646b8cbdc7df2da
KadenPang <156834084+KadenPang@users.noreply.github.com>
2024-04-19 14:03:18 -0400

    savetextinputs

--- CodeFiles/TextInput with variable answer.cs
+++ CodeFiles/TextInput with variable answer.cs
@@ -12,26 +12,36 @@ public class InputHandler : MonoBehaviour
     //Create a public float so that the answer can be defined within Unity
     public float ans;
 
-    public GameObject bruh;
+    public GameObject door;
+    /**
+    Asks for a string input and checks if it is the correct float
+    */
     public void ValidateInput()
     {
+        //Store the user's input as a string in a variable
         string input = inputField.text;
 
         try
         {
+            //Try to convert the input into a float and store it as the result variable
             float result = float.Parse(input);
             
+            //If the result is equal to the global variable ans which is defined in Unity
             if (result == ans)
             {
+                //Display "Correct" to the user and deactivate the door block
                 resultText.text = "Correct";
-                bruh.SetActive(true);
+                door.SetActive(false);
             }
 
+            //As long as the input is a float but not the correct answer, display "Incorrect"
             else
             {
                 resultText.text = "Incorrect";
             }
         }
+
+        //While the user does not have a float, display "Enter a float"
         catch
         {
             resultText.text = "Enter a float";
--- CodeFiles/TextInput.cs
+++ CodeFiles/TextInput.cs
@@ -1,4 +1,5 @@
 // https://www.youtube.com/watch?v=EogkNlAkMI8
+//Older version of getting text input with only one valid answer
 
 using System.Collections;
 using System.Collections.Generic;
@@ -7,13 +8,16 @@ using UnityEngine.UI;
 
 public class InputHandler : MonoBehaviour
 {
+    //Creating fields for attaching an inputField and a text for displaying text in Unity
     [SerializeField] InputField inputField;
     [SerializeField] Text resultText;
 
     public void ValidateInput()
     {
+        //Store the user's input from the inputfield
         string input = inputField.text;
 
+        //If the input is 0.553 as a string, display correct, otherwise display incorrect
         if (input == "0.553")
         {
             resultText.text = "Correct";
