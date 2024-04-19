7c50ca1a659cad9180db2bf8b8c38f543cbdc6e3
Daw <133544883+DawDa07@users.noreply.github.com>
2024-04-19 13:41:36 -0400

    Update SceneManagerScript.cs

--- CodeFiles/SceneManagerScript.cs
+++ CodeFiles/SceneManagerScript.cs
@@ -24,26 +24,32 @@ public class SceneController : MonoBehaviour
 
     public void NextRoom()
     {
+        //start going to the next room
         StartCoroutine(LoadLevel());
         //SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadScene(string sceneName)
     {
+    //start going to a specified room
         //SceneManager.LoadSceneAsync(sceneName);
         StartCoroutine(LoadLevelspec(sceneName));
     }
 
     IEnumerator LoadLevelspec(string room)
     {
+        //start the animation to change rooms
         transitionAnim.SetTrigger("End");
+        //wait for one second
         yield return new WaitForSeconds(1);
+        //load new scene
         SceneManager.LoadSceneAsync(room);
         transitionAnim.SetTrigger("Start");
     }
 
     IEnumerator LoadLevel()
     {
+        //play animation to go to the room
         transitionAnim.SetTrigger("End");
         yield return new WaitForSeconds(1);
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
