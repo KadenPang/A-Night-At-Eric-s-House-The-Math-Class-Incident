e0adf7a511b114ed4ade26db25b6d5e3bbb48407
KadenPang <156834084+KadenPang@users.noreply.github.com>
2024-05-01 14:20:20 -0400

    savescenetracker

--- CodeFiles/SceneTracker.cs
+++ CodeFiles/SceneTracker.cs
@@ -9,9 +9,12 @@ public class sceneTracker : MonoBehaviour
     void Update()
     {
         Scene lastScene = SceneManager.GetActiveScene();
+        //If the current scene is not the main menu
         if (lastScene.name != "Main Menu")
         {
+            // Gets the current scene the player is in as a game object
             Scene currentScene = SceneManager.GetActiveScene();
+            // Turns the game object into the string of the currentName variable to be accessed
             currentName = currentScene.name;
         }
         
