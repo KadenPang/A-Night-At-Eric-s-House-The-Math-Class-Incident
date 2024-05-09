57e53e4398394cd1ebff0f026a76e4b4771c90ac
KadenPang <156834084+KadenPang@users.noreply.github.com>
2024-05-09 12:53:45 -0400

    save

--- CodeFiles/DawsonDIalogue.cs
+++ CodeFiles/DawsonDIalogue.cs
@@ -25,6 +25,7 @@ public class DawsonDialogue : MonoBehaviour
         health = GameManager.gameManager._playerHealth.Health;
         
       
+        //If the 
         if (Input.GetKeyUp(KeyCode.Space) && playerIsClose)
         {
             if (dialoguePanel.activeInHierarchy)
@@ -48,12 +49,14 @@ public class DawsonDialogue : MonoBehaviour
 
     public void zeroText()
     {
+        //If the player loses all their health, deactivate the dialogue panel
         if (health <= 0)
         {
             dialoguePanel.SetActive(false);
         }
         else
         {
+            //Remove all the text and the fields with text in them and reset everything so that the next time this is activated, it is blank again
             dialogueText.text = "";
             index = 0;
             dialoguePanel.SetActive(false);
@@ -65,6 +68,7 @@ public class DawsonDialogue : MonoBehaviour
 
     IEnumerator Typing()
     {
+        //For every character in the element that is being printed right now, wait for wordSpeed seconds until printing the next letter
         foreach (char letter in dialogue[index].ToCharArray())
         {
             if (dialoguePanel.activeInHierarchy)
@@ -88,6 +92,7 @@ public class DawsonDialogue : MonoBehaviour
         }
     }
 
+    /*When the player sprite collides with the object that this file is attached to, playerIsClose = true*/
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -101,6 +106,7 @@ public class DawsonDialogue : MonoBehaviour
         }
     }
 
+    /*When the player sprite is no longer in contact with the collider of this object, playerIsClose = false and the text is erased using the zerotext() function*/
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
