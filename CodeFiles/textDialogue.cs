41282a41d70f8c171fb2bd72a8e4b9c8c1834858
KadenPang <156834084+KadenPang@users.noreply.github.com>
2024-05-18 14:28:12 -0400

    commentsTextdialogue

--- CodeFiles/textDialogue.cs
+++ CodeFiles/textDialogue.cs
@@ -1,20 +1,37 @@
-//LINK TO YOUTUBE TUTORIAL https://www.youtube.com/watch?v=1nFNOyCalzo 
+/*
+ICS4U
+Kaden Pang
+This program is meant for dialogue coming from NPCs. Code was taken from online and used for learning and testing purposes. 
+The modified version that is specific to A Night at Eric's house is in the DawsonDialogue.cs file
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextDialogue : MonoBehaviour
+public class TextDialogue : MonoBehaviour //[1]
 {
+    //Create public variables for attaching the appropriate game objects. Game objects will need to be created and assigned
+    //Should be a UI Panel
     public GameObject dialoguePanel;
+    //Should be a UI text (legacy)
     public Text dialogueText;
+    //This will be the actual text that should be printed
     public string[] dialogue;
-    private int index;
 
+
+    //Should be a UI inputfield (legacy)
+    public GameObject inputField;
+    //Should be a UI button
     public GameObject contButton;
 
+    //Float that represents how long the program should wait before printing out the next character
     public float wordSpeed;
+    //Bool for whether or not the player sprite is nearby
+    //The game object that this is attached to should already have some sort of collider component
     public bool playerIsClose;
+    //Refers to the amount of different text prompts there are (how many times the continue button can change the text)
+    private int index;
 
     // Update is called once per frame
     void Update()
@@ -54,6 +71,7 @@ public class TextDialogue : MonoBehaviour
         dialoguePanel.SetActive(false);
     }
 
+    //Use IEnumerator to be used with Coroutine, which would allow for the function Typing() to be stopped while it is still executing
     IEnumerator Typing()
     {
         //For every character that is in the text to be displayed
@@ -65,10 +83,14 @@ public class TextDialogue : MonoBehaviour
         }
     }
 
+    /*Used to move on to the next line in the index
+    Meant to be attached to the continue button in Unity*/
     public void NextLine()
     {
+        //Disable the continue button so that the player has to wait until all of the code has been printed out before going to the next set of text
         contButton.SetActive(false) ;
 
+        //If there is more text to be displayed after pressing the continue, increase the index, erase the text and start typing out the next element
         if(index < dialogue.Length -1) 
         {
             index++;
@@ -77,7 +99,7 @@ public class TextDialogue : MonoBehaviour
         }
     }
 
-
+    /*When the player sprite collides with the object that this file is attached to, playerIsClose = true, otherwise keep the dialogue panel blank*/
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -87,10 +109,11 @@ public class TextDialogue : MonoBehaviour
 
         else
         {
-            zeroText() ;
+            zeroText();
         }
     }
 
+    /*When the player sprite is no longer in contact with the collider of this object, playerIsClose = false and the text is erased using the zerotext() function*/
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -100,3 +123,9 @@ public class TextDialogue : MonoBehaviour
         }
     }
 }
+
+/*
+Bibliography
+[1] diving_squid, “Unity 2D NPC Dialogue System tutorial,” YouTube, https://www.youtube.com/watch?v=1nFNOyCalzo (accessed May 17, 2024). 
+*/
+
