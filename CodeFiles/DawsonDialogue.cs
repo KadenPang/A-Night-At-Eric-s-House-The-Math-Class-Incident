64065201bb9f6855bc2bc6666a242e4aec94c68e
KadenPang <156834084+KadenPang@users.noreply.github.com>
2024-05-18 14:06:40 -0400

    CommentsOnDawsonDialogue

--- CodeFiles/DawsonDialogue.cs
+++ CodeFiles/DawsonDialogue.cs
@@ -1,5 +1,9 @@
 /*
-
+ICS4U
+Kaden Pang,Daw Da, Chingis Toktamyssov
+IN UNITY, this program runs when the user presses the space key while close to whatever this object this file is a component of. 
+Displays to the user some text that which is entered as an element within Unity
+Built off of the textDialogue.cs file
 */
 using System.Collections;
 using System.Collections.Generic;
@@ -8,25 +12,36 @@ using UnityEngine.UI;
 
 public class DawsonDialogue : MonoBehaviour
 {
+    //Create public variables for attaching the appropriate game objects. Game objects will need to be created and assigned
+    //Should be a UI Panel
     public GameObject dialoguePanel;
+    //Should be a UI text (legacy)
     public Text dialogueText;
+    //This will be the actual text that should be printed
     public string[] dialogue;
-    private int index;
 
+
+    //Should be a UI inputfield (legacy)
     public GameObject inputField;
-
-
+    //Should be a UI button
     public GameObject contButton;
 
+    //Float that represents how long the program should wait before printing out the next character
     public float wordSpeed;
+    //Bool for whether or not the player sprite is nearby
+    //The game object that this is attached to should already have some sort of collider component
     public bool playerIsClose;
+    //int variable for storing the health of the player
     int health;
 
+    //Refers to the amount of different text prompts there are (how many times the continue button can change the text)
+    private int index;
+    
     // Update is called once per frame
     void Update()
     {
+        //Access the value to be assigned as the health of the player through the game manager
         health = GameManager.gameManager._playerHealth.Health;
-        
       
         //If the user presses space and playerIsClose (meaning the player sprite is touching this game object)
         if (Input.GetKeyUp(KeyCode.Space) && playerIsClose)
@@ -89,10 +104,14 @@ public class DawsonDialogue : MonoBehaviour
         }
     }
 
+    /*Used to move on to the next line in the index
+    Meant to be attached to the continue button in Unity*/
     public void NextLine()
     {
+        //Disable the continue button so that the player has to wait until all of the code has been printed out before going to the next set of text
         contButton.SetActive(false);
 
+        //If there is more text to be displayed after pressing the continue, increase the index, erase the text and start typing out the next element
         if (index < dialogue.Length - 1)
         {
             index++;
