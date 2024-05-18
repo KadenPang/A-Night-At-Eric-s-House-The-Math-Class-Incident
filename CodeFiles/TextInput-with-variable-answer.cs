44020a6b34d38464f74f5712bd72c8634754d4d8
KadenPang <156834084+KadenPang@users.noreply.github.com>
2024-05-18 16:23:42 -0400

    commentsfortextinput

--- CodeFiles/TextInput-with-variable-answer.cs
+++ CodeFiles/TextInput-with-variable-answer.cs
@@ -1,3 +1,16 @@
+/*
+ICS4U
+Kaden Pang, Daw Da
+This is a more modular version of the TextInput.cs file, where the valid input is defined within the Unity game engine.
+
+History:
+Upload date: March 29, 2024
+
+References:
+[1] Used for learning how to access input fields and text boxes and the proper syntax for if statements in c#
+[2] Used for learning how to convert string to float in C#
+[3] Used for learning Try statements in C#
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +18,27 @@ using UnityEngine.UI;
 
 public class InputHandler : MonoBehaviour
 {
-    //Create fields for attaching an input field and a text object in Unity
+    //Create fields for attaching an input field and a text object in Unity [1]
     [SerializeField] InputField inputField;
     [SerializeField] Text resultText;
 
     //Create a public float so that the answer can be defined within Unity
     public float ans;
 
+    //Create a public game object that will be disabled if the valid input is entered
     public GameObject door;
-    /**
-    Asks for a string input and checks if it is the correct float
-    */
+
+
+    /*Asks for a string input and checks if it is the correct float*/
     public void ValidateInput()
     {
         //Store the user's input as a string in a variable
         string input = inputField.text;
 
-        try
+        try //[3]
         {
-            //Try to convert the input into a float and store it as the result variable
-            float result = float.Parse(input);
+            //Try to convert the input into a float and store it as the result variable [2]
+            float result = float.Parse(input); 
             
             //If the result is equal to the global variable ans which is defined in Unity
             if (result == ans)
@@ -41,7 +55,7 @@ public class InputHandler : MonoBehaviour
             }
         }
 
-        //While the user does not have a float, display "Enter a float"
+        //While the user does not enter a float, display "Enter a float"
         catch
         {
             resultText.text = "Enter a float";
@@ -52,3 +66,10 @@ public class InputHandler : MonoBehaviour
     }
 
 }
+
+/*
+Bibliography
+[1] M. O’Didily, “How to get user input and validate it using Unity,” YouTube, https://www.youtube.com/watch?v=EogkNlAkMI8 (accessed May 17, 2024). 
+[2] B. Wagner, “How to convert a string to a number - C#,” How to convert a string to a number - C# | Microsoft Learn, https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number (accessed May 17, 2024). 
+[3] [1] “C# exceptions - try..catch,” C# Exceptions (Try..Catch), https://www.w3schools.com/cs/cs_exceptions.php (accessed May 17, 2024). 
+*/
--- CodeFiles/TextInput.cs
+++ CodeFiles/TextInput.cs
@@ -1,6 +1,13 @@
-// https://www.youtube.com/watch?v=EogkNlAkMI8
-//Older version of getting text input with only one valid answer
+/*
+ICS4U
+Kaden Pang
+Older version of getting text input and validating it (only one valid answer).
+TextInput-Health.cs is the file used in the final game
+References
+[1] Used for learning how to access input fields and text boxes and the proper syntax for if statements in c#
 
+
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +15,16 @@ using UnityEngine.UI;
 
 public class InputHandler : MonoBehaviour
 {
-    //Creating fields for attaching an inputField and a text for displaying text in Unity
+    //Creating fields for attaching an inputField and a text for displaying text in Unity [1]
     [SerializeField] InputField inputField;
     [SerializeField] Text resultText;
 
     public void ValidateInput()
     {
-        //Store the user's input from the inputfield
+        //Store the user's input from the inputfield [1]
         string input = inputField.text;
 
-        //If the input is 0.553 as a string, display correct, otherwise display incorrect
+        //If the input is 0.553 as a string, display correct, otherwise display incorrect [1]
         if (input == "0.553")
         {
             resultText.text = "Correct";
@@ -32,3 +39,7 @@ public class InputHandler : MonoBehaviour
     }
 
 }
+/*
+Bibliography
+[1] M. O’Didily, “How to get user input and validate it using Unity,” YouTube, https://www.youtube.com/watch?v=EogkNlAkMI8 (accessed May 17, 2024). 
+*/
